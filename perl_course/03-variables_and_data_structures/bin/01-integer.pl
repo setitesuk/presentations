45460632f7f3ce427ceb753acae64401b87c86d9
Andy Brown <ajb@sanger.ac.uk>
2011-02-07 16:20:47 +0000

    slide text

--- perl_course/03-variables_and_data_structures/bin/01-integer.pl
+++ perl_course/03-variables_and_data_structures/bin/01-integer.pl
@@ -4,6 +4,8 @@ use Modern::Perl;
 
 say 1; # integers don't need quoting
 
+### Maths
+
 say 1+1; # addition
 
 say 2*7; # multiplication
@@ -16,9 +18,13 @@ say 3**2; # square
 
 say 4**4; # (4 to the power of 4)
 
+### do something that affects strings ordinarily
+
 say 1 . 1; # this is a string function, so coerces the integers into strings, and then performs the functions
 
 say 0;
+
+### boolean numbers
 
 say 1 if 0; # this is boolean untrue
  
@@ -28,6 +34,22 @@ say '0 but true' + 25; # because of 0, it is still able to do number functions, 
 
 say 3 if '0'; # this is still false
 
+say 4 if 4; # any number other than 0 is true
+
+### floating points
+
+say 1.00; # truncates past the decimal point
+
+say '1.00'; # acts as a string
+
+say 1.01;
+
+### producing sequences
+
+say 0..9; # generates a sequence of numbers
+
+### numerical testing
+
 say 4 if ( 1 == 1 ); # lhv and rhv are equal
 
 say 5 if ( 1 != 2 ); # lhv and rhv are not equal
@@ -36,17 +58,12 @@ say 6 if ( 2 > 1 ); # lhv greater than rhv
 
 say 7 if ( 1 < 2 ); # rhv less than lhv
 
-say 1.00; # truncates past the decimal point
+# also >= and =<
 
-say '1.00'; # acts as a string
-
-say 1.01;
-
-say 0..9; # generates a sequence of numbers
+### comparisons
 
 say 10 <=> 11; # the comparison or 'spaceship' operator
 say 10 <=> 10;
 say 11 <=> 10;
 
-# also >= and =<
 
--- perl_course/03-variables_and_data_structures/bin/03-scalar.pl
+++ perl_course/03-variables_and_data_structures/bin/03-scalar.pl
@@ -2,6 +2,8 @@
 
 use Modern::Perl;
 
+### Assignment
+
 my $integer_scalar = 10;
 
 my $string_scalar = 'I am a string';
@@ -9,11 +11,15 @@ my $string_scalar = 'I am a string';
 say $integer_scalar;
 say $string_scalar;
 
+### What do I do with a scalar?
+
 say $string_scalar . $integer_scalar; # string concatenation
 
 say $integer_scalar + $integer_scalar; # numeric addition
 
 say $string_scalar * $integer_scalar; # gives warning that string_scalar isn't numeric, converts to 0 and so answer 0
+
+### Mix scalars and fixed values
 
 say 'I can use a fixed string and concatenate to ' . $string_scalar;
 
@@ -22,7 +28,19 @@ say 30 / $integer_scalar;
 # what do you thing the following would do
 say $string_scalar x $integer_scalar; # x is a 'return this x times feature'
 
+### Interpolate scalars
+
 say 'Hello, ' . $string_scalar;
 say 'Hello, $string_scalar'; # single quotes won't interpolate the scalar
 say 'Hello, ' . $string_scalar;
 say "Hello, $string_scalar"; # double quotes will interpolate the scalar
+
+### undef
+
+my $undef_scalar;
+
+say $undef_scalar;
+
+$string_scalar = undef;
+
+say $string_scalar if $string_scalar; # we have cleared $string scalar, so we won't print anything
