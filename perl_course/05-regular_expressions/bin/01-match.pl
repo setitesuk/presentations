3aa5f6bdb997eeed5f194306e4d8d8aac0dff364
Andy Brown <ajb@sanger.ac.uk>
2011-04-13 15:28:04 +0100

    slides for regular expressions - dropped x and s as not really appropriate

--- perl_course/05-regular_expressions/bin/01-match.pl
+++ perl_course/05-regular_expressions/bin/01-match.pl
@@ -9,25 +9,25 @@ my $sequence = 'CCGGATCACTATGACCTGCTTTCGCACCTGCTCGCGCCGTCACGCTCGCAGTC';
 
 say $sequence;
 
-if ( $sequence =~ m/ATG/xms ) {
+if ( $sequence =~ m/ATG/i ) {
   say 'Sequence contains Methionine';
 }
 
 # does it contain a number (\d represents a digit, + is 1 or more)
 
-if ( $sequence =~ m/\d+/xms ) {
+if ( $sequence =~ m/\d+/ ) {
   say 'Sequence contain a number';
 }
 
 # does it not contain a number ( !~ is does not match operator)
 
-if ( $sequence !~ m/\d+/xms ) {
+if ( $sequence !~ m/\d+/ ) {
   say 'Sequence does not contain a number';
 }
 
 # does it contain a word character ( \w )
 
-if ( $sequence =~ m/\w+/xms ) {
+if ( $sequence =~ m/\w+/ ) {
   say 'Sequence contains word characters';
 }
 
@@ -35,73 +35,50 @@ if ( $sequence =~ m/\w+/xms ) {
 
 my $lyrics = 'I see a little silhoutte of a man';
 
-if ( $lyrics =~ m/\s/xms ) {
+if ( $lyrics =~ m/\s/ ) {
   say 'Lyrics contain whitespace';
 }
 
 # we can capture information from the regex
 
-my ( $silhoutte ) = $lyrics =~ m/\s(silhoutte)\s/ixms;
+my ( $silhoutte ) = $lyrics =~ m/\s(silhoutte)\s/i;
 
 say $silhoutte;
 
 # we can even capture all words like this
 # capture the first word
-my ( $first_word ) = $lyrics =~ m/(\w+)\s+/xms;
+my ( $first_word ) = $lyrics =~ m/(\w+)\s+/;
 
 say $first_word;
 
 # capture last word (\z matched end of string, * means 0 or more)
 
-my ( $last_word ) = $lyrics =~ m/\s(\w+)\s*\z/xms; 
+my ( $last_word ) = $lyrics =~ m/\s(\w+)\s*\z/m; 
 say $last_word;
 
 # regex + and * are greedy (they match the maximum that they can)
 # my $sequence = 'CCGGATCACTATGACCTGCTTTCGCACCTGCTCGCGCCGTCACGCTCGCAGTC';
 
-my ( $greedy_match ) = $sequence =~ /(\w+ACC)/xms;
+my ( $greedy_match ) = $sequence =~ /(\w+ACC)/;
 say $greedy_match;
 
 # to make not greedy, add ?
 
-my ( $non_greedy_match ) = $sequence =~ /(\w+?ACC)/xms;
+my ( $non_greedy_match ) = $sequence =~ /(\w+?ACC)/;
 say $non_greedy_match;
 
 # we can match user defined matches
 
 my $user_wants_to_find_this_sequence = 'ttt';
 
-if ( $sequence =~ /$user_wants_to_find_this_sequence/ixms ) {
+if ( $sequence =~ /$user_wants_to_find_this_sequence/i ) {
   say "User sequence $user_wants_to_find_this_sequence found.";
 }
-
-####### what do ixms represent?
+####### what do i and m represent?
 
 # what are the letters afterwards
 
 # i - ignore case (obviously, be sure you want this)
-# x - This allows you to split up complex regexes into readble lines, with comments
-
-# i.e.
-
-my $string_to_match = q{'single quoted string'};
-
-if ( $string_to_match =~ m{'[^\\']*(?:\\.[^\\']*)*'} ) {
-  say "$string_to_match matches a single quoted string";
-}
-
-if ( $string_to_match =~ m{ ' # an opening single quote
-      [^\\']*                 # any non-special chars ( i.e., not backslash or single quote)
-      (?:                     # then all of...
-        \\ .                  #   any explicitly backslashd char 
-        [^\\']*               #   followed by any non-special chars
-      )*                      # ...repeated zero or more times
-      '                       # a clossing single quote
-    }x ) {
-      
-  say 'now you can read how it matched';
-}
-
 # m - line boundaries ^ and $ mean different things in perl to other languages. to make it consistent,
 # the m flag means ^ match at the start of any line, and $ means match at the end of any line
 # (ordinarily, they mean match at start and end of the string explicitly)
@@ -119,32 +96,14 @@ should be expected
 
 ';
 
-unless ( $text =~ m/(^Here.*expected$)/xs ) {
+unless ( $text =~ m/(^Here.*expected$)/ ) {
   say 'The text did not match, as ^ is start of string and $ is end of the string';
 }
 
-if ( $text =~ m/(^Here.*?expected$)/xms ) {
+if ( $text =~ m/(^Here.*?expected$)/m ) {
   say 'The text matched is as ^ is start of line and $ is end of a line';
 }
 
 # If you want to explicitly state match start of string, use m/\AHello/xms
 # If you want to explicitly state match end of string,   use m/Goodbye\z/xms
 
-
-# s - make . truly match anything, ordinarily it won't match a newline character
-
-# i.e.
-
-my $match_anything = "Hello\nWorld\tnice to see you, to see you\n\nnice!";
-
-unless ( $match_anything =~ m/Hello.*nice\!/xm ) {
-  say 'we failed to match newlines without the s flag';
-}
-if ( $match_anything =~ m/Hello.*nice\!/xms ) {
-  say 'we matched newlines with the s flag';
-}
-
-
-# return value is boolean return 1 or undef
-say $match_anything =~ m/Hello.*nice\!/xms; # true
-say $match_anything =~ m/Hello.*nice\!/xm; # false
--- perl_course/05-regular_expressions/bin/02-substitute.pl
+++ perl_course/05-regular_expressions/bin/02-substitute.pl
@@ -4,15 +4,15 @@ use Modern::Perl;
 
 my $sequence = 'CCGGATCACTATGACCTGCTTTCGCACCTGCTCGCGCCGTCACGCTCGCAGTC';
 
-unless ( $sequence =~ m/cat/ixms ) {
+unless ( $sequence =~ m/cat/i ) {
   say 'we do not have a cat';
 }
 
 # we want a cat
 
-$sequence =~ s/cac/cat/ixms;
+$sequence =~ s/cac/cat/i;
 
-if ( $sequence =~ m/cat/ixms ) {
+if ( $sequence =~ m/cat/i ) {
   say 'we now have a cat';
 }
 
@@ -23,7 +23,7 @@ say $sequence; # 1 - how many cac's were there? 3
 # note it did the first one it came to
 # how do we get 3 cats - use /g (globally substitute)
 
-$sequence =~ s/cac/cat/gixms;
+$sequence =~ s/cac/cat/gi;
 
 say $sequence;
 
@@ -36,27 +36,24 @@ say $sequence;
 my $favourite_codon = 'aaa'; # it's all the first letter of my name;
 my $least_favourite_codon = 'cat'; # I don't actually like cats
 
-$sequence =~ s/${least_favourite_codon}/$favourite_codon/gixms;
+$sequence =~ s/$least_favourite_codon/$favourite_codon/gi;
 
 say $sequence;
-
-# the xms act ensure that your substitution match part act exactly the same as the match would do
-# you don't want to set up a substitute dependent on a match, which could have 2 different results
 
 # the return value is the number of substitutions made, or undef if none are made
 # because of the false value for no substitutions, you can use as a boolean
 
-say $sequence =~ s/aaa/cac/ixms; # 1 (we didn't specify global)
-say $sequence =~ s/aaa/cac/gixms; # 2 (the remaining ones)
-say $sequence =~ s/cat/aaa/gixms; # undef, we had already cleared away all the cats
+say $sequence =~ s/aaa/cac/i; # 1 (we didn't specify global)
+say $sequence =~ s/aaa/cac/gi; # 2 (the remaining ones)
+say $sequence =~ s/cat/aaa/gi; # undef, we had already cleared away all the cats
 
 # in substituting, watch out for greediness
 
 my $clone = $sequence;
 
-$sequence =~ s/g.*cac/aaa/ixms;
+$sequence =~ s/g.*cac/aaa/i;
 say $sequence;
-$clone =~ s/g.*?cac/aaa/ixms;
+$clone =~ s/g.*?cac/aaa/i;
 say $clone;
 # note that we wouldn't have been able to do multiple substitutions,
 # since the first one was the first g matched to the last cac
